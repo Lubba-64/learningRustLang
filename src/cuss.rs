025963cfb9a72c31ce726c91377a8751a694de1c
Benjamin Hines <75751722+Lubba-64@users.noreply.github.com>
2022-04-03 01:57:40 -0400

    fix: use proper syntax for infinite loop

--- src/cuss.rs
+++ src/cuss.rs
@@ -2,7 +2,7 @@ use crate::my_io;
 
 pub fn run(){
     println!("Listen, you're allowed to cuss or whatever, say whatever the fuck you want I guess...");
-    while true {
+    loop {
         my_io::getln(true);
     }
 }
